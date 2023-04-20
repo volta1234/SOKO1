0e56efb9ae98522cc050200f97c3e57c0c42736e
CALVINCE <calvince.ogwel@gmail.com>
2023-04-20 10:08:45 +0300

    users resources.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
     end
 
     def create
-        @user = @user.new(user_params)
+        @user = User.new(user_params)
         if @user.save
             render json: @user, status: :created
         else
